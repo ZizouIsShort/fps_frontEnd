b08bea5beba52869ec2d8206977d7050d58cd41a
Ziyan Solkar <ziyansolkar@gmail.com>
2025-05-11 01:16:37 +0400

    omg my dabba rotates

--- src/components/tester.tsx
+++ src/components/tester.tsx
@@ -1,14 +1,31 @@
 "use client"
 
-import {Canvas} from "@react-three/fiber";
+import {Canvas, useFrame} from "@react-three/fiber";
+import {useRef} from "react";
+import * as THREE from "three";
+
+function Dabba(){
+    const boxref = useRef(null);
+    useFrame(
+        () => {
+            boxref.current.rotation.x += 0.005;
+            boxref.current.rotation.y += 0.005;
+            boxref.current.rotation.z += 0.005;
+        }
+    )
+    return (
+        <mesh ref={boxref}>
+            <boxGeometry args={[2, 2, 2]}/>
+            <meshBasicMaterial color={"gold"}/>
+        </mesh>
+    )
+}
+
 export default function Mkc(){
     return (
         <>
-            <Canvas camera={{position: [2, 2, 2]}}>
-                <mesh>
-                    <boxGeometry args={[2, 3, 2]} />
-                    <meshBasicMaterial color={"gold"}/>
-                </mesh>
+            <Canvas>
+                <Dabba/>
             </Canvas>
         </>
     )
