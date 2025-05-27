c517947079df43df652b1909bada5b5577e3115f
Ziyan Solkar <ziyansolkar@gmail.com>
2025-05-28 02:24:29 +0400

    sent a get request to the express server to test if it works

--- src/app/maardomujhe/page.tsx
+++ src/app/maardomujhe/page.tsx
@@ -1,4 +1,16 @@
+"use client"
+
 export default function IKILLMYSELF() {
+    const wtfa = async () => {
+        try {
+            const data = await fetch('http://localhost:3002/nice');
+            const response = await data.json();
+            console.log(response);
+        } catch (error) {
+            console.error('Fetch error:', error);
+        }
+    }
+
     return (
         <>
             <h1>
@@ -6,6 +18,7 @@ export default function IKILLMYSELF() {
             </h1>
             <div>
                 <p>Yeah this is a fake commit to continue the streak</p>
+                <button onClick={wtfa} className={'bg-green'}>MKC</button>
             </div>
         </>
 
